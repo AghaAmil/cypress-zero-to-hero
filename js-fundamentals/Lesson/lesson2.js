47fae04d12737f9b856f227688aed2f622a63068
AghaAmil <moravveji.amir.92@gmail.com>
2024-12-07 19:40:24 +0330

    js variable & datatypes

---
+++ js-fundamentals/Lesson/lesson2.js
@@ -0,0 +1,42 @@
+/*
+
+Variables
+
+ */
+
+
+var firstName;               // declare the variable
+firstName = 'Amirhossein';   // initiate the variable
+
+var lastName = 'Moravveji';      // declare and initiate the variable
+
+var age, job, postion;
+age = 30;
+job = 'QA Engineer';
+postion = 'L3G1';
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(job);
+console.log(postion);
+
+// ES6 variables
+// let
+let name = 'Amirhossein Moravveji';
+console.log(name);
+
+name = 'Shiva Rajabi';       // change the variable value
+console.log(name);
+
+// constant variable
+const PI = 3.14;
+console.log(PI);
+
+// cannot change the value a constant variable
+// PI = 3.20
+
+
+
+
+
