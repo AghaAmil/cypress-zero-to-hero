edf003a875f9707bc6b8e9040119189caf3f04da
AghaAmil <moravveji.amir.92@gmail.com>
2024-12-10 18:58:45 +0330

    creating first cypress script

---
+++ ngx-cypress-test/cypress/e2e/firstTest.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('First Test Suite', () => {
+
+  it('First Test', () => {
+    // put the code of the test
+  })
+
+})
---
+++ ngx-cypress-test/cypress/e2e/projectStructureSample.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress"/>
+
+describe('First Test Suite', () => {
+
+  describe('Suite Section', () => {
+
+    beforeEach('Login', () => {
+      // repeat for every test
+    })
+
+    it('First Test', () => {
+      // put the code of the test
+    })
+
+    it('Second Test', () => {
+      // put the code of the test
+    })
+
+  })
+
+  it('First Test', () => {
+    // put the code of the test
+  })
+
+  it('Second Test', () => {
+    // put the code of the test
+  })
+
+  it('Third Test', () => {
+    // put the code of the test
+  })
+
+})
+
+describe('Second Test Suite', () => {
+
+  it('First Test', () => {
+    // put the code of the test
+  })
+
+  it('Second Test', () => {
+    // put the code of the test
+  })
+
+  it('Third Test', () => {
+    // put the code of the test
+  })
+
+})
